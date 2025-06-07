8019b5e4f6e1411e9f63f056257df75b7b5eacaa
Bionicfpss <murilo.agrela29@gmail.com>
2025-06-07 14:57:56 -0300

    Add files via upload

---
+++ operações.py
@@ -0,0 +1,16 @@
+def main():
+  num1 = int(input("Digite o primeiro numero: "))
+  num2 = int(input("Digite o segundo numero: "))
+
+  soma = num1 + num2
+  sub = num1 - num2
+  mult = num1 * num2
+  div = num1 / num2
+
+  print("a soma de ", num1, " + ", num2, " = ", soma)
+  print("a subtracao de ", num1, " - ", num2, " = ", sub)
+  print("a multiplicacao de ", num1, " * ", num2, " = ", mult)
+  print("a divisao de ", num1, " / ", num2, " = ", div)
+        
+  return 0
+main()
